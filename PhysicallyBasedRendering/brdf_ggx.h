efcd2116ae24174182afbe8beb217731588c858a
park jee hyeok <wlgur01014@naver.com>
2017-12-20 12:52:04 +0900

    new commit

---
+++ PhysicallyBasedRendering/brdf_ggx.h
@@ -0,0 +1,64 @@
+#ifndef _BRDF_GGX_
+#define _BRDF_GGX_
+
+#include "brdf.h"
+
+class BrdfGGX : public Brdf
+{
+public:
+
+	// pdf 값이 0보다 크면 eval / pdf값을 사용
+	// pdf값과 어떤 값을 리턴하는군
+	virtual float eval(const vec3& V, const vec3& L, const float alpha, float& pdf) const
+	{
+		// 상반구에 대해서 값을 구하고 있기 때문에
+		// V.z가 0보다 작으면 pdf 값과 리턴값을 0으로 함
+		if(V.z <= 0)
+		{
+			pdf = 0;
+			return 0;
+		}
+
+		// masking
+		const float a_V = 1.0f / alpha / tanf(acosf(V.z));
+		const float LambdaV = (V.z<1.0f) ? 0.5f * (-1.0f + sqrtf(1.0f + 1.0f/a_V/a_V)) : 0.0f;
+		const float G1 = 1.0f / (1.0f + LambdaV);
+
+		// shadowing
+		float G2;
+		if(L.z <= 0.0f)
+			G2 = 0;
+		else
+		{
+			const float a_L = 1.0f / alpha / tanf(acosf(L.z));
+			const float LambdaL = (L.z<1.0f) ? 0.5f * (-1.0f + sqrtf(1.0f + 1.0f/a_L/a_L)) : 0.0f;
+			G2 = 1.0f / (1.0f + LambdaV + LambdaL);
+		}
+
+		// D
+		const vec3 H = normalize(V+L);
+		const float slopex = H.x/H.z;
+		const float slopey = H.y/H.z;
+		float D = 1.0f / (1.0f + (slopex*slopex+slopey*slopey)/alpha/alpha);
+		D = D*D;
+		D = D / (3.14159f * alpha * alpha * H.z*H.z*H.z*H.z);
+
+		pdf = fabsf(D * H.z / 4.0f / dot(V,H));
+		float res = D * G2 / 4.0f / V.z;
+
+		return res;
+	}
+
+	virtual vec3 sample(const vec3& V, const float alpha, const float U1, const float U2) const
+	{
+		const float phi = 2.0f * 3.14159f * U1;
+		const float r = alpha*sqrtf(U2/(1.0f-U2));
+		const vec3 N = normalize(vec3(r*cosf(phi), r*sinf(phi), 1.0f));
+		// reflect ray
+		const vec3 L = -V + 2.0f * N * dot(N, V);
+		return L;
+	}
+};
+
+
+#endif

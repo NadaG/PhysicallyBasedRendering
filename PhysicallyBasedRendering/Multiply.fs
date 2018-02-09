d51ad8f8df0e514a1fbd31d1a95153b313946d1a
park jee hyeok <wlgur01014@naver.com>
2018-02-09 17:24:12 +0900

    glare...

--- PhysicallyBasedRendering/Multiply.fs
+++ PhysicallyBasedRendering/Multiply.fs
@@ -9,9 +9,21 @@ out vec3 color;
 uniform sampler2D apertureTex;
 uniform sampler2D fresnelDiffractionTex;
 
+float Fresnel(float value)
+{
+	float lambda = 0.3f;
+	float d = 0.1f;
+
+	float x = (outUV.x - 0.5f)*0.5f;
+	float y = (outUV.y - 0.5f)*0.5f;
+	float ret = cos(pi / (lambda * d) * (x*x + y*y)) * value;
+	
+	return ret;
+}
+
 void main()
 {	
-	float value = texture(apertureTex, outUV).r;
+	float value = texture(apertureTex, outUV).r ;
 
-	color = vec3(value);
+	color = vec3(Fresnel(value));
 }
